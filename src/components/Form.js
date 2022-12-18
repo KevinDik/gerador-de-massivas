eb3181b8b8a31adb9bcee6383a0ef1212fb4b4ee
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-12-18 09:48:12 -0300

    feat: Reestruturando o projeto, retirado react

--- src/components/Form.js
+++
@@ -1,8 +0,0 @@
-import styles from './Form.module.css';
-
-export default function Form() {
-    return(
-        <form className={styles.form}>
-            <h1>Gerador de massivas</h1>
-        </form>)
-}
--- src/components/Select.js
+++
@@ -1,9 +0,0 @@
-export default function Select() {
-    return(
-        <div>
-            <select>
-                <option></option>
-            </select>
-        </div>
-        )
-}
