2e3467d3e8ae9fa23f61ba262c056c568f97578a
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-12-24 18:55:10 -0300

    style: Criado elementos after e before e definido variveis JS

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let empresa = document.querySelector("#empresa");
+let eventualidade = document.querySelector("#evento");
+let cidade = document.querySelector("#cidade");
+let responsavel = document.querySelector("#setor");
+let hora_inicio = document.querySelector("#hora");
+let quantidade_clientes = document.querySelector("#quant_clientes");
